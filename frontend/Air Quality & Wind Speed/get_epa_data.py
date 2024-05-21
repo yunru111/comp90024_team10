2d589595fd97db863aecc563d65ffaa18541558c
yunru111 <yunruz@student.unimelb.edu.au>
2024-05-22 09:56:46 +1000

    Add files via upload

---
+++ frontend/Air Quality & Wind Speed/get_epa_data.py
@@ -0,0 +1,26 @@
+####Comp90024_group10
+###Shanrui Huang - 1533562
+###Lingyi Zhang - 1470460
+###Jiaxing Wang - 1511557
+###Yunru Zhu - 1470423
+
+import json
+from elasticsearch import Elasticsearch
+
+def search_epa(es, index):
+    query = {
+        "query": {
+            "term": {
+                "site_name.keyword": "Mildura"
+            }
+        }
+    }
+
+    response = es.search(index=index, body=query, size=10000)
+    hits = response['hits']['hits']
+    with open("epa_data.json", "w") as f:
+        for hit in hits:
+            json.dump(hit["_source"], f)
+            f.write("\n")
+    data = [hit['_source'] for hit in hits]
+    return data
